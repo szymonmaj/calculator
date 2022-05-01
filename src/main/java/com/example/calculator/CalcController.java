d67ea6d23cdb1f147eb15560dc4fc3c544e43e10
sim <jasm0012@interia.pl>
2022-05-01 16:34:13 +0200

    new commit3

--- src/main/java/com/example/calculator/CalcController.java
+++ src/main/java/com/example/calculator/CalcController.java
@@ -2,17 +2,61 @@ package com.example.calculator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Controller
 public class CalcController {
+    @GetMapping("/")
+    public String showForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "calc";
+    }
 
-@GetMapping("/")
-    public String calc(Model model){
-    model.addAttribute("hello","mamama");
-    return "calc";
+    @PostMapping("/")
+    public String submitForm(@ModelAttribute("user") User user) {
+        System.out.println(user);
+        return "register_success";
+    }
+
+
+//    public double a = 10;
+//    public double b = 5;
+//@GetMapping("/")
+//    public String calc(Model model){
+//    double c = a + b;
+//    model.addAttribute("hello","mamama");
+//    model.addAttribute("c", c);
+//    model.getAttribute("d");
+//    return "calc";
+//}
+//@GetMapping("/")
+//    public String adding(Model model){
+//    double c = a + b;
+//    model.addAttribute("c", "c");
+//    return "calc";
+//    }
+// TextField textField = new TextField();
+// String fdg = textField.getText();
+//@RequestMapping("/")
+//public String index(
+//        @RequestParam(value = "participant", required = false) String participant,
+//        @RequestParam(value = "country", required = false) String country,
+//        @RequestParam(value = "action", required = false) String action,
+//        @RequestParam(value = "id", required = false) Integer id,
+//        Model model
+//) {
+//    model.addAttribute("id", id);
+//    List<Integer> userIds = asList(1,2,3,4);
+//    model.addAttribute("userIds", userIds);
+//    return "calc";
+//}
 }
 
 
-}
---
+++ src/main/java/com/example/calculator/User.java
@@ -0,0 +1,36 @@
+package com.example.calculator;
+
+public class User {
+private int a;
+private int b;
+public int add(){
+    c = a + b;
+    return c;
+};
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+    private int c;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+}
